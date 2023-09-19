47c784919719930b30ad8acba28862b9530b14f5
Derrick0094 <145224533+Derrick0094@users.noreply.github.com>
2023-09-19 11:24:00 -0500

    Update main.py

--- Day 1/main.py
+++ Day 1/main.py
@@ -2,10 +2,10 @@
 print("Hello and welcome to the band name creator!")
 
 #2. Ask the user for the city that they grew up in.
-city = input("In what city did you grow up? ")
+city = input("In what city did you grow up?"\n)
 
 #3. Ask the user for the name of a pet.
-pet = input("what is the name of your favorite pet? ")
+pet = input("what is the name of your favorite pet?"\n)
 
 #4. Combine the name of their city and pet and show them their band name.
 band_name = city + " " + pet
